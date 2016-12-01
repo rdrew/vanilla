103dfe90c41aad527b46c857a83a417515cbf1fb
Rob Drew <rdrew@upei.ca>
2016-12-01 01:13:03 -0400

    1st

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var importer = require('node-sass-globbing');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+
+var sass_config = {
+	importer: importer,
+	includePaths: [
+		//'node_modules/breakpoint-sass/stylesheets/',
+		//'node_modules/singularitygs/stylesheets/',
+		//'node_modules/compass-mixins/lib/'
+	]
+};
 
 gulp.task('sass:prod', function () {
   gulp.src('./sass/*.scss')
@@ -17,7 +29,7 @@ gulp.task('sass:prod', function () {
 gulp.task('sass:dev', function () {
   gulp.src('./sass/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass(sass_config).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 version']
     }))
